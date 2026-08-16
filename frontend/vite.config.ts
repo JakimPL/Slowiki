35f08c90f56d1aa554cae69260e8ec59f37be3d2
JakimPL <jakimpl@gmail.com>
2026-08-16 20:05:38 +0200

    Minor improvements

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "../build/frontend",
-    emptyOutDir: true,
-  },
-  server: {
-    proxy: {
-      "/offerings": "http://127.0.0.1:8000",
-      "/tables": "http://127.0.0.1:8000",
+    plugins: [react()],
+    build: {
+        outDir: "../build/frontend",
+        emptyOutDir: true,
     },
-  },
+    server: {
+        proxy: {
+            "/offerings": "http://127.0.0.1:8000",
+            "/tables": "http://127.0.0.1:8000",
+        },
+    },
 });
